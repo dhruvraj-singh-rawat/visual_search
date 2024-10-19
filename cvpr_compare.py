aab2c190987307f71a35c4f0735655a1dc01cf61
Dhruvraj Singh Rawat <dhruvraj.rawat@gmail.com>
2024-10-19 20:04:24 +0100

    Initial Commit

---
+++ cvpr_compare.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def cvpr_compare(F1, F2,type='L2'):
+    # This function compares F1 to F2 - i.e. compute the distance
+    # between the two descriptors
+    # extended the capacity of this function to calc both L1 and L2 distance 
+    
+    if type == 'L2':
+        F_diff = F1-F2
+        F_diff_sqr = np.square(F_diff)
+        l2_norm = np.sqrt(np.sum(F_diff_sqr))
+        return l2_norm
+    
+    if type == 'L1':
+        l1_norm = np.sum(np.absolute(F1 - F2))
+        return l1_norm
+
+    return None
+
+
---
+++ cvpr_computehist.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+# Takes normalised image between 0.0 - 1.0 
+# Q is number of Bin 
+
+def calculate_histogram_bins(image, Q):
+    binned_image = np.clip(np.floor(Q * image).astype(int), 0, Q - 1)
+
+    # Split the channels
+    red_channel = binned_image[:, :, 0]
+    green_channel = binned_image[:, :, 1]
+    blue_channel = binned_image[:, :, 2]
+
+    # Calculate the histogram for each channel, with a bin count of Q
+    red_hist = np.bincount(red_channel.flatten(), minlength=Q)
+    green_hist = np.bincount(green_channel.flatten(), minlength=Q)
+    blue_hist = np.bincount(blue_channel.flatten(), minlength=Q)
+
+    return red_hist, green_hist, blue_hist
